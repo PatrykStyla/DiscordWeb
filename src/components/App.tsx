b9fdcab9f837c4da0d02044a166b85ac4fdaf2af
Patryk Styla <43710236+ws295203@users.noreply.github.com>
2021-05-28 01:54:40 +0000

    Primitive logout functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,17 +21,6 @@ import { GuildLogs } from './Dashboard/GuildLogs';
 // 	</div>
 // );
 
-
-async function GetUser() {
-
-	const response = await fetch('/api/users/@me',
-		{
-			method: 'GET',
-		}
-	)
-	return response.json()
-}
-
 function GuildElement(props: {guild_id: string}) {
 	let history = useHistory()
 
@@ -49,30 +38,33 @@ function GuildElement(props: {guild_id: string}) {
 function App() {
 	const [user, setUser] = useState<{guild_id: string, permissions: number, icon: number, name: string}[] | null>(null)	
 	const [a, setA] = useState<string | null>(null)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
 
 	useEffect(() => {
 		fetch('/api/users/@me')
 			.then(res => res.json())
 			.then((result) => {
 				setUser(result)
+				setIsLoggedIn(true);
 			},
 			(error) => {
 				console.log(error)
 			})
 	},[])
 
-	if (user) {	
+	if (isLoggedIn && user) {	
 		let Guilds: JSX.Element[] = []
 		user.forEach((element,key) => {
 			Guilds.push(<GuildElement key={element.guild_id} guild_id={element.guild_id}/>)
 		})
 		return (
 		<>
-			<NavBar />
+			<NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
 			<Switch>
 				<Route exact path="/">
 					{Guilds}
+					<div className=""></div>
 				</Route>
 				<Route exact path="/server/:guild_id">
 					{/* User selected a guild. Show a dashboard */}
@@ -89,7 +81,7 @@ function App() {
 		</>	
 		)
 	} else {
-		return (<NavBar></NavBar>)
+		return (<NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></NavBar>)
 	}
 
 }
